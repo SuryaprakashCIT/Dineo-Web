b1020b2c6b6740774d7b0949844b3e291f8bf6e2
Suryaprakash M <sprakash29395@gmail.com>
2017-11-14 17:29:18 +0530

    Surya : Slack Webhooks Added.

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,16 +1,43 @@
 import axios from 'axios';
 
+const slackWebhook = (message) => {
+    return (dispatch) =>{
+        axios.post('https://hooks.slack.com/services/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', message)
+        .then( (response) => {
+            if(response.data === "Done"){
+                console.log("Success");
+            }
+        }).catch( (exception) => {
+           setTimeout(() => {
+               dispatch(closeSnack())
+             }, 2500);
+       })
+   }
+}
+
 const contactUs = (userDetails) => {
-    console.log("Recieved userdetails : ", userDetails.name);
     return (dispatch) =>{
          axios.post('http://www.dineo.in/newContact', userDetails)
          .then( (response) => {
              if(response.data === "Done"){
                  console.log("Success");
                  dispatch(postSuccess());
-                 setTimeout(() => {
-                    dispatch(closeSnack())
-                  }, 2500);
+                if(userDetails.name !== ''){
+                    dispatch(slackWebhook(
+                        {
+                            "text": "*"+userDetails.name+"* wants to get in touch with us. \n Says "+userDetails.message+"\n *Contact details :* "+userDetails.email,
+                        }
+                    ));
+                } else {
+                    dispatch(slackWebhook(
+                        {
+                            "text": "A new user is interested in us : *"+userDetails.email+"*",
+                        }
+                    ));
+                }
+                setTimeout(() => {
+                dispatch(closeSnack())
+                }, 2500);
              }
          }).catch( (exception) => {
             console.log(exception);
@@ -18,8 +45,8 @@ const contactUs = (userDetails) => {
             setTimeout(() => {
                 dispatch(closeSnack())
               }, 2500);
-         })
-        }
+        })
+    }
 }
 const postSuccess =() => {
     return { type: 'POST_SUCCESS', };
@@ -27,7 +54,6 @@ const postSuccess =() => {
 const closeSnack =() => {
     return { type: 'CLOSE_SNACK', };
 };
-
 
 const postFailure= () =>{
     return {
